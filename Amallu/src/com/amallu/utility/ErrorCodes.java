038c39ff080983abf872705c16f9411ea67fdb3f
krishna <enkrao@gmail.com>
2016-02-21 16:23:12 +0530

    removed error codes

--- Amallu/src/com/amallu/utility/ErrorCodes.java
+++ Amallu/src/com/amallu/utility/ErrorCodes.java
@@ -15,36 +15,4 @@ public class ErrorCodes {
 	public static final String CONNECTION_EXCEPTION = "103";
 	public static final String SECURITY_EXCEPTION = "104";
 	
-	//Error Codes for User/Admin Signup service.
-	public static final String REG_DETS_NOT_AVAIL_ERR_CODE="700101";
-	public static final String REG_EMAIL_NOT_AVAIL_ERR_CODE="700102";
-	public static final String REG_MOBNO_NOT_AVAIL_ERR_CODE="700103";
-	public static final String REG_UNAME_NOT_AVAIL_ERR_CODE="700104";
-	public static final String REG_UTYPE_NOT_AVAIL_ERR_CODE="700105";
-	public static final String REG_EMAIL_NOT_PROP_FMT_ERR_CODE="700106";
-	public static final String REG_MOBNO_LENGTH_ERR_CODE="700107";
-	public static final String REG_ALREADY_AVAIL_ERR_CODE="700108";
-	public static final String REG_UTYPE_INVALID_ERR_CODE="700109";
-	public static final String REG_UNAME_AVAIL_ERR_CODE="700110";
-	public static final String REG_EMAIL_AVAIL_ERR_CODE="700111";
-	public static final String REG_MOBNO_AVAIL_ERR_CODE="700112";
-	public static final String REG_MAX_LENGTH_EXCEEDED_ERR_CODE="700113";
-	
-	//Error Codes for ValidateOTP service.
-	public static final String VALIDATEOTP_OTP_NOT_AVAIL_ERR_CODE="700201";
-	public static final String VALIDATEOTP_INVAL_OTP_NOT_AVAIL_ERR_CODE="700202";
-	
-	//Error Codes for FetchTokens service.
-	public static final String TOKENS_NOT_AVAIL_ERR_CODE="700301";
-	
-	//Error Codes for FetchItems service.
-	public static final String ITEMS_NOT_AVAIL_ERR_CODE="700401";
-	
-	//Error Codes for FetchProfile service.
-	public static final String FETCH_PROFILE_USER_NOT_AVAIL_ERR_CODE="700501";
-	public static final String FETCH_PROFILE_INVALID_USER_ERR_CODE="700502";
-	
-	//Error Codes for FetchProfile service.
-	public static final String UPDATE_PROFILE_USER_NOT_AVAIL_ERR_CODE="700601";
-	public static final String UPDATE_PROFILE_INVALID_USER_ERR_CODE="700602";
 }
